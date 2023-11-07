8265e88d88cefb1188340bc83c41f5454f682040
JhonnyJ <jhv.bss@gmail.com>
2023-11-07 10:16:16 -0500

    07.11.2023

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: AppTheme().theme(),
+      theme: AppTheme(selectedColor: 5).theme(),
       debugShowCheckedModeBanner: false,
       title: 'Yes no app',
       home: Scaffold(
